14d483f58b2e021df748e06d45686054f4faf0b2
rezamini <reza.amini.b@gmail.com>
2023-05-06 13:27:40 +0100

    - added a new images directory to be used for... a main path to test and detect landmark images. - moved the testing images under images/ directory - modified detect_circle_landmarks to be a class with relevant functions.

--- detect_circle_landmarks.py
+++ detect_circle_landmarks.py
@@ -2,45 +2,62 @@ import matplotlib.pyplot as plt
 import numpy as np
 import cv2 as cv
 from detect_landmark_color import DetectLandmarkColor
-import math
 from apicalls import ApiCalls
 
-original_image = cv.imread("example_shapes9_orange.png")
-output_image = original_image.copy()
-gray = cv.cvtColor(original_image, cv.COLOR_BGR2GRAY)
-hist = cv.equalizeHist(gray)
-blur = cv.GaussianBlur(hist, (31,31), cv.BORDER_DEFAULT)
-height, width = blur.shape[:2]
-minR = round(width/65)
-maxR = round(width/11)
-minDis = round(width/7)
-circles = cv.HoughCircles(blur, cv.HOUGH_GRADIENT, 1, minDis, param1=14, param2=30, minRadius=minR, maxRadius=maxR)
-detectLandmarkColor = DetectLandmarkColor()
-api = ApiCalls()
-colorResult = set()
+class DetectCircleLandmarks:
+    draw_result = True
+    detectLandmarkColor = DetectLandmarkColor()
+    api = ApiCalls()
+    color_result = set()
 
-# Draw circles
-if circles is not None:
-    detected_circles = np.uint16(np.around(circles))
-    for (x,y,r) in detected_circles[0, :]:
-        landmark_color, bgr_color = detectLandmarkColor.detect_color(original_image, y, x)
-        colorResult.add(landmark_color.lower())
-        
-        cv.circle(output_image, (x,y), r, (255,255,255), 3) #outer circle
-        # cv.circle(output_image, (x, y), 2, (255, 255, 255), 3)# inner circle/dot
-        
-        cv.putText(output_image, landmark_color, (x+r, y+r+r),
-            cv.FONT_HERSHEY_SIMPLEX, 0.5, (int(bgr_color[0]), int(bgr_color[1]), int(bgr_color[2])), 2)
+    def __init__(self, drawResult):
+        self.draw_result = drawResult
 
+    def detect_circles(self, image_path, image_name):
+        original_image = cv.imread(image_path + image_name)
+        output_image = original_image.copy()
+        gray = cv.cvtColor(original_image, cv.COLOR_BGR2GRAY)
+        hist = cv.equalizeHist(gray)
+        blur = cv.GaussianBlur(hist, (31,31), cv.BORDER_DEFAULT)
+        height, width = blur.shape[:2]
+        minR = round(width/65)
+        maxR = round(width/11)
+        minDis = round(width/7)
+        circles = cv.HoughCircles(blur, cv.HOUGH_GRADIENT, 1, minDis, param1=14, param2=30, minRadius=minR, maxRadius=maxR)
+        self.__perform_color_detection(image_name, original_image, output_image, circles)
 
+    
+    def __perform_color_detection(self, image_name, original_image, output_image, circles):
+        if circles is not None:
+            temp_color_result = set()
+            detected_circles = np.uint16(np.around(circles))
+            for (x,y,r) in detected_circles[0, :]:
+                landmark_color, bgr_color = self.detectLandmarkColor.detect_color(original_image, y, x)
+                temp_color_result.add(landmark_color.lower())
+                
+                #modify output image if draw_result is true
+                if self.draw_result:
+                    cv.circle(output_image, (x,y), r, (255,255,255), 3) #outer circle
+                    # cv.circle(output_image, (x, y), 2, (255, 255, 255), 3)# inner circle/dot
+                    cv.putText(output_image, landmark_color, (x+r, y+r+r),
+                        cv.FONT_HERSHEY_SIMPLEX, 0.5, (int(bgr_color[0]), int(bgr_color[1]), int(bgr_color[2])), 2)
 
-# cv.circle(original_image, (422, 198), 1, (0, 0, 0), 3)
-# print(hsv_frame[198, 422])
+        #add detected colors to the main result  
+        self.color_result.update(temp_color_result)
 
-api.send_data(colorResult)
+        #print the detected colors for every image
+        print("------------ Detected Colors for file [" + image_name + "] ------------")
+        print(temp_color_result)
 
-cv.imshow("result", np.hstack([original_image, output_image]))
-cv.waitKey(0)
-cv.destroyAllWindows()
+        #display image differences if draw_result is true
+        if self.draw_result:
+            self.draw_detected_landmarks(original_image, output_image)
+
+    def draw_detected_landmarks(self, original_image, output_image):
+
+        # self.api.send_data(self.colorResult)
+        cv.imshow("result", np.hstack([original_image, output_image]))
+        cv.waitKey(0)
+        cv.destroyAllWindows()
 
 
